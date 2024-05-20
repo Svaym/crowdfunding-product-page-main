6722d73bdf6d3d63b2c61fa5052a224a31218315
Svaym <Vaymeraa22@mail.ru>
2024-05-20 17:35:35 +0500

    change mobileMenu

--- src/components/Header/NavLinks.jsx
+++ src/components/Header/NavLinks.jsx
@@ -1,8 +1,8 @@
 
 export default function NavLinks() {
   return (
-    <nav className="xxs:bg-white xxs:w-2/3 xxs:h-[12%] xxs:rounded-lg xxs:absolute xxs:top-16 xxs:p-3 xxs:left-1/2 xxs:-translate-x-1/2 xxs:animate-fade">
-      <ul className="text-white flex items-center gap-x-5 font-medium xs:text-[15px] xxs:text-base xxs:text-black xxs:flex-col xxs:gap-y-3">
+    <nav className="xxs:bg-white xxs:w-2/3 xxs:h-24 xxs:rounded-lg xxs:absolute xxs:top-16 xxs:p-2 xxs:left-1/2 xxs:-translate-x-1/2 xxs:animate-fade">
+      <ul className="text-white flex items-center gap-x-5 font-medium xs:text-[15px] xxs:text-base xxs:text-black xxs:flex-col xxs:gap-y-1">
         <li>
           <a className="transition duration-200 ease-linear hover:contrast-50" href="#">About</a>
         </li>
